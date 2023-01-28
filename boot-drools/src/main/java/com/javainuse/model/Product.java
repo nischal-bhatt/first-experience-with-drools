34de74fd2d16448ebf1e6705a6908e47de066689
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-01-28 18:07:44 +0800

    drools

---
+++ boot-drools/src/main/java/com/javainuse/model/Product.java
@@ -0,0 +1,35 @@
+package com.javainuse.model;
+
+public class Product {
+
+	private String type;
+	private String typekwando;
+	private int discount;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public String getTypekwando() {
+		return typekwando;
+	}
+
+	public void setTypekwando(String typekwando) {
+		this.typekwando = typekwando;
+	}
+	
+	
+
+}
